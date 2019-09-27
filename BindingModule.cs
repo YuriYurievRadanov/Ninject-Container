66ea235cb2c75ec98150996d0b8fc0b78b97f9d8
YAVUZ KILIÇ <yavuz.kilic@koton.com.tr>
2019-09-27 15:42:13 +0300

    Ninject

---
+++ BindingModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ninject.Modules;
+
+namespace DINinject
+{
+    class BindingModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IEncryptor>().To<MichalengeloEncryptor>();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Reflection;
+using Ninject;
+using Ninject.Modules;
+
+namespace DINinject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Without Ninject
+
+            //IEncryptor _encryptor = new DaVinciEncryptor();
+            //MessageProvider provider = new MessageProvider(_encryptor);
+            //string encryptedMessage = provider.EncryptMessage(""You'll never walk"");
+            //string decryptedMessage = provider.DecryptMessage(Liverpool); 
+
+            //Console.WriteLine(encryptedMessage);
+            //Console.WriteLine(decryptedMessage);
+
+            #endregion
+
+
+            #region With Ninject 
+
+            IKernel kernel = new StandardKernel();
+            kernel.Load(new BindingModule());
+            IEncryptor _encryptor = kernel.Get<IEncryptor>();
+
+            MessageProvider provider = new MessageProvider(_encryptor);
+            string encryptedMessage = provider.EncryptMessage("You'll never walk");
+            string decryptedMessage = provider.DecryptMessage("Liverpool");
+
+            Console.WriteLine(encryptedMessage);
+            Console.WriteLine(decryptedMessage);
+
+            #endregion
+        }
+    }
+
+    class MessageProvider
+    {
+        private IEncryptor _encryptor;
+        public MessageProvider(IEncryptor Encryptor)
+        {
+            _encryptor = Encryptor;
+        }
+        public string EncryptMessage(string Message)
+        {
+            return _encryptor.Encrypt(Message + " alone.");
+        }
+        public string DecryptMessage(string Message)
+        {
+            Message = "";
+            Message = "You'll walk alone.";
+            return _encryptor.Decrypt(Message);
+        }
+    }
+
+    interface IEncryptor
+    {
+        string Encrypt(string Message);
+        string Decrypt(string Message);
+    }
+
+    class MichalengeloEncryptor : IEncryptor
+    {
+        public string Decrypt(string Message)
+        {
+            return Message;
+        }
+
+        public string Encrypt(string Message)
+        {
+            return Message;
+        }
+    }
+
+    class DaVinciEncryptor : IEncryptor
+    {
+        public string Decrypt(string Message)
+        {
+            return Message;
+        }
+
+        public string Encrypt(string Message)
+        {
+            return Message;
+        }
+    }
+
+    class MessageBindingModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IEncryptor>().To<DaVinciEncryptor>();
+        }
+    }
+}
